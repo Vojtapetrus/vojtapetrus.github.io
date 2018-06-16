d95154bfe6dd871b3ae982fde03c54caa3887ed9
Vojtapetrus <vojta.petrus@me.com>
2018-06-16 19:19:21 +0100

    a lot of changes

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
-   })
+})
 
- $(document).ready(function() {
+var $navBar = null;
+var $mushrooms = null;
 
-    $(".navbar").hide(); //Hide the navigation bar first
-  
+$(document).ready(function() {
+    
+    $navBar = $(".scrollnav")[0];
+    $mushrooms = $("section#mushrooms")[0];
+
+    //$(".navbar").hide(); //Hide the navigation bar first
+    if($(window).width() > 768){
+
+
+
     $(window).scroll(function() { //Listen for the window's scroll event
-      if (isScrolledAfterElement("#mushrooms")) { //if it has scrolled beyond the #content elment
-        $('.navbar').fadeIn(); //Show the navigation bar
-      } else {
-        $('.navbar').fadeOut(); //Else hide it
+      if ($navBar != undefined && $navBar != null ) {
+        if (isScrolledAfterElement($mushrooms)) { //if it has scrolled beyond the #content elment
+            $navBar.style.transform = 'translateY(0)';
+        } else {
+            $navBar.style.transform = 'translateY(-100px)';
+        }
       }
     });
   
     //Function that returns true if the window has scrolled beyond the given element
-    function isScrolledAfterElement(elem) {
-      var $elem = $(elem);
-      var $window = $(window);
+    function isScrolledAfterElement($elem) {
   
-      var docViewTop = $window.scrollTop();
-      var docViewBottom = docViewTop + $window.height();
-  
-      var elemTop = $elem.offset().top;
-  
-      return elemTop <= docViewBottom;
+      var elemTop = $elem.getBoundingClientRect().top;
+
+      return elemTop < 0;
     }
-  });
+  } else {
+    $navBar.style.transform = 'translateY(0)'}
+});
+
